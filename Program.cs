d3c068da24f298d29b8bac82b4651b44853d7f67
--global <--global>
2024-10-04 22:01:08 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int idade;
+bool podeSerDoador;
+
+Console.Clear();
+Console.Write("Qual a sua idade: ");
+
+idade = Convert.ToInt32(Console.ReadLine()!);
+podeSerDoador = (idade >= 18) && (idade <= 67);
+
+if (podeSerDoador)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Você pode ser doador de sangue.");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Você não pode ser doador de sangue.");
+}
+
+Console.ResetColor();
